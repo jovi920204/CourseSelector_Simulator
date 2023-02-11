e78406b84f2cdeb7f17cd29610280a6bebbd8136
jovi <73748197+jovi920204@users.noreply.github.com>
2023-02-12 00:59:04 +0800

    GET First/A01/A01 to show result

--- app.js
+++ app.js
@@ -8,9 +8,24 @@ var course = require('./course.json');
 
 const app = express();
 
+var status = "Normal";
+// "Normal"  一般狀況
+// "Success"  成功
+// "Conflict"  衝堂
+// "Duplicated"  重複選課
+// "NotExist"  課程不存在
+// "Full"  人數已滿
+// "NotOpen"  這門課遴選不開放選修，所以無法選修。
+// "ExceedCredit" 如果修這門課將會造成選課學分數超過上限(超修)，所以無法選修
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/views/B01.html");
 })
+
+app.get("/First/A01/A01", function(req, res){
+    res.sendFile(__dirname + "/views/A03_" + status+ ".html");
+    status = "Normal";
+});
 
 app.post("/AddAndSub/B01/ExtraJoin", function(req, res){
     const courseNo = req.query.CourseNo;
@@ -38,26 +53,30 @@ app.post("/AddAndSub/B01/ExtraJoin", function(req, res){
             });
             return flag;
         }()){
-            console.log("A03_Conflict 衝堂");
-            res.sendFile(__dirname + "/views/A03_Conflict.html");
+            status = "Conflict";
+            // console.log(status + "衝堂");
+            // res.sendFile(__dirname + "/views/A03_Conflict.html");
         }
         // 沒有衝堂 => 成功
         else{
             course.courseChooseList.push(targetCourse);
-            console.log("A03_Success 成功加選");
-            res.sendFile(__dirname + "/views/A03_Success.html");
+            status = "Success";
+            // console.log(status + "成功加選");
+            // res.sendFile(__dirname + "/views/A03_Success.html");
         }
     }
     
     // 有這門課 但 選課清單已經有了 （重複選課）
     else if (course.courseData.find(i => i.courseNo === courseNo) && course.courseChooseList.find(i => i.courseNo === courseNo)){
-        console.log("A03_Duplicated 重複選課");
-        res.sendFile(__dirname + "/views/A03_Duplicated.html");
+        status = "Duplicated";
+        // console.log(status + "重複選課");
+        // res.sendFile(__dirname + "/views/A03_Duplicated.html");
     }
     // 沒有這門課 （課程不存在）
     else{
-        console.log("A03_NotExist 課程不存在");
-        res.sendFile(__dirname + "/views/A03_NotExist.html");
+        status = "NotExist";
+        // console.log(status + "課程不存在");
+        // res.sendFile(__dirname + "/views/A03_NotExist.html");
     }
 
     // 更新資料
